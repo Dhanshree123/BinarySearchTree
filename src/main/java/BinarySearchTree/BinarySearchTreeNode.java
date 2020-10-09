81a8281ce5fcbf29673022d140172cc15f31ea25
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-10-09 12:24:44 +0530

    Added classes to add nodes to BST

---
+++ src/main/java/BinarySearchTree/BinarySearchTreeNode.java
@@ -0,0 +1,15 @@
+package BinarySearchTree;
+
+public class BinarySearchTreeNode<K extends Comparable<K>> {
+
+	K key;
+	BinarySearchTreeNode<K> leftNode;
+	BinarySearchTreeNode<K> rightNode;
+
+	public BinarySearchTreeNode(K key) {
+		this.key = key;
+		this.leftNode = null;
+		this.rightNode = null;
+	}
+
+}
